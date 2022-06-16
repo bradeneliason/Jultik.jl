913924e3192c934a8dc9368f22c0fa6452feaf5e
Braden Eliason <github@bradeneliason.com>
2022-06-15 20:09:00 -0500

    initial commit

--- src/Jultik.jl
+++ src/Jultik.jl
@@ -1,5 +1,18 @@
 module Jultik
+    using HTTP
+    using Gumbo
+    using Cascadia
+    using DataFrames
 
-# Write your package code here.
+    include("types.jl")
+    include("crawl.jl")
+    include("scrape.jl")
+
+    export TextSelector, AttrSelector, HTMLSelector, LinkSelector, PairSelector
+    export Spider, SpiderResult
+    export Schema, Field
+
+    export crawl!, urls
+    export scrape
 
 end
